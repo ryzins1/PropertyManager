a03b27a3017baa67b6c49e2d45781a7a3288d420
dzins <dzins@mac.com>
2014-09-09 21:08:53 -0500

    rename

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -10,16 +10,6 @@ namespace MvcApplication.Controllers
     {
         public ActionResult Index()
         {
-			var mongoDb = MongoDatabase.Create(ConfigurationManager.AppSettings["MongoDBTimesheets"]);
-			var repository = mongoDb.GetCollection<Timesheet>(typeof(Timesheet).Name);
-			var timesheets = new List<Timesheet>
-            {
-                new Timesheet { FirstName = "Christophe", LastName = "Geers", Month = 8, Year = 2012},
-                new Timesheet { FirstName = "Ruben", LastName = "Geers", Month = 8, Year = 2012 }
-            };
-			foreach (var timesheet in timesheets)
-				repository.Insert(timesheet);
-
             return View();
         }
 
