2430ffae2cb18bf675502465d8084e0e5e79b19e
dzins <dzins@mac.com>
2014-09-10 22:07:04 -0500

    updates

--- MvcApplication/App_Start/WebApiConfig.cs
+++ MvcApplication/App_Start/WebApiConfig.cs
@@ -27,11 +27,6 @@ namespace MvcApplication
 			// Web API routes
 			config.MapHttpAttributeRoutes();
 
-			config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
         }
     }
 
