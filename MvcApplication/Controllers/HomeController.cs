fc71d43e21d8b415702a0034e3dd407f93c9892e
dzins <dzins@mac.com>
2014-09-11 01:19:34 -0500

    updates

--- MvcApplication/Controllers/HomeController.cs
+++ MvcApplication/Controllers/HomeController.cs
@@ -1,18 +1,39 @@
-﻿using System.Web;
+﻿using System.Configuration;
+using System.Linq;
 using System.Web.Mvc;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+using MvcApplication.Models;
 
 namespace MvcApplication.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+		private readonly MongoCollection<Building> _buildingRepository;
+
+	    public HomeController()
+	    {
+			var connectionString = ConfigurationManager.ConnectionStrings["PropertyManager"].ConnectionString;
+			var client = new MongoClient(connectionString);
+			var server = client.GetServer();
+			var mongoDb = server.GetDatabase("PropertyManager");
+
+			_buildingRepository = mongoDb.GetCollection<Building>(typeof(Building).Name);
+	    }
+
+		public ActionResult Index()
         {
             return View();
         }
 
 		public ActionResult Units(string id)
 		{
+			var query = Query.EQ("_id", id);
+			var building = _buildingRepository.Find(query).Single();
+
 			ViewBag.Id = id;
+			ViewBag.BuildingName = building.Name;
+
 			return View();
 		}
 		
